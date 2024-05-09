2b46793a325fbaf54d812e5b8359921faef1ceec
Nick Nielsen <nielsenn@purdue.edu>
2024-05-09 11:41:14 -0500

    repeat string complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let total_string = "";
+    for (let i = 0; i < times; i++) {
+        total_string +=word;
+    }
+    return total_string;
 };
 
 // Do not edit below this line
