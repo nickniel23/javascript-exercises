37518aa3078e9c3070007c57f087a5b32cc5ae6d
Nick Nielsen <nielsenn@purdue.edu>
2024-05-09 12:52:10 -0500

    remove from array complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(initial_array, ...args) {
+    const new_array = [];
 
+    initial_array.forEach(element => {
+        
+        if (!args.includes(element)) {
+            new_array.push(element);
+        }
+    });
+
+    return new_array;
 };
 
 // Do not edit below this line
