41a6d0ee337af5607b8676f7956b413b9d076313
Nick Nielsen <nielsenn@purdue.edu>
2024-05-09 19:27:45 -0500

    complete calculator js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(number_1, number_2) {
+  let result;
+
+  result = number_1 + number_2;
+
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(number_1, number_2) {
+  let result;
+
+  result = number_1 - number_2;
+
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const sum_of_arr = arr.reduce((total, current_value) => {
+    return total + current_value;
+  }, 0);
+
+  return sum_of_arr;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  const product_of_arr = arr.reduce((total, current_value) => {
+    return total * current_value;
+  }, 1);
 
+  return product_of_arr;
 };
 
-const power = function() {
-	
+const power = function(number_1, number_2) {
+	power_number = number_1 ** number_2;
+
+  return power_number;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (!number) return 1;
+  let total;
+  total = 1;
+  for (let i = 1; i <= number; i++)
+    {
+      total *= i;
+    }
+  return total;
 };
 
 // Do not edit below this line
@@ -31,3 +56,5 @@ module.exports = {
   power,
   factorial
 };
+
+
