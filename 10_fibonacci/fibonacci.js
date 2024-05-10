4550a0b11ec44c476bf6ba70e0865616fa1a71dc
Nick Nielsen <nielsenn@purdue.edu>
2024-05-09 22:16:53 -0500

    fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if (typeof count !== "number") {
+        count = parseInt(count);
+    }
 
+    if (count < 0) return "OOPS";
+
+    let fib_array = [0, 1];
+
+    for (let i = 2; i <= count; i++) {
+        fib_array.push(fib_array[i - 1] + fib_array[i - 2]);
+    }
+
+    return fib_array[count];
 };
 
 // Do not edit below this line
