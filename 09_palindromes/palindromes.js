068eaa39cf0c9836ea9132342d8c31c1bfb0b8ff
Nick Nielsen <nielsenn@purdue.edu>
2024-05-09 20:12:15 -0500

    palindrome complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    let characters = word.split("");
+    characters = characters.filter(is_not_punctuation);
+    word = characters.join("");
 
+    let reverse = characters.reverse();
+    reverse = reverse.join("");
+    return word === reverse;
 };
+
+const is_not_punctuation = function (character) {
+    let punctuations = [".", ",", ":", "!", "?", " "];
+    return !(punctuations.includes(character));
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
