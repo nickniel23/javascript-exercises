feea95c6315a653af241a36c32e89c156d060b3d
Nick Nielsen <nielsenn@purdue.edu>
2024-05-09 22:26:10 -0500

    get titles complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(book_array) {
+    book_title_array = book_array.map( (book) => book.title);
+    return book_title_array;
 };
 
 // Do not edit below this line
